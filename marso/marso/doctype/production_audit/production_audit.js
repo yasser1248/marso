fce00fd4ef583c6385e40c729303824b8bc5aaf3
yasser1248 <yasserelbana@gmail.com>
2023-08-24 15:39:25 +0300

    update doctypes

--- marso/marso/doctype/production_audit/production_audit.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, basel waheed and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Production Audit', {
-	// refresh: function(frm) {
-
-	// }
-});
--- marso/marso/doctype/production_audit/test_production_audit.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, basel waheed and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestProductionAudit(unittest.TestCase):
-	pass
